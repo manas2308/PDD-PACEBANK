e3e030c8c09a03dbc9b02331882e5bd934ea4048
Manas Reddy <118883728+manas2308@users.noreply.github.com>
2025-03-17 23:32:12 +0530

    Delete SAMPLE/staff directory

--- SAMPLE/staff/conf/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $dbuser="root";
-    $dbpass="";
-    $host="localhost";
-    $db="banking";
-    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
--- SAMPLE/staff/staff_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['admin_id']);
-    session_destroy();
-
-    header("Location:../staff/staff_login.php");
-    exit;
